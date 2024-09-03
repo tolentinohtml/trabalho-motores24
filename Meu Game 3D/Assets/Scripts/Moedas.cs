1370c247d318fbecfebeb5deba64192e8727016b
tolentinohtml <luizguilhermetolentino0099@gmail.com>
2024-09-03 14:27:16 -0300

    nova

---
+++ Meu Game 3D/Assets/Scripts/Moedas.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Moedas : MonoBehaviour
+{
+    public int velocidadeGiro = 50;
+ private void OnTriggerEnter(Collider other){
+    if(other.gameObject.tag == "Player"){
+        Destroy(gameObject);
+    }
+ }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * velocidadeGiro * Time.deltaTime);
+    }
+}
