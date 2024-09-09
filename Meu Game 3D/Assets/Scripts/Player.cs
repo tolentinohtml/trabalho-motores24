fa2f3a3565f91b00b075ea1b7f618ad0b459713a
tolentinohtml <luizguilhermetolentino0099@gmail.com>
2024-09-09 08:34:57 -0300

    yygygyg

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,54 +1,55 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Player : MonoBehaviour
-private AudioSource source;
 {
-    public int velocidade =  10;
+    public int velocidade = 10;
     public int forcaPulo = 7;
     private Rigidbody rb;
+    private AudioSource source;
     public bool noChão;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(message:"START");
-        TryGetComponent (out rb);
+        UnityEngine.Debug.Log("START");
+        TryGetComponent(out rb);
         TryGetComponent(out source);
     }
-     private void OnCollisionEnter(Collision collision){
-        if(!noChão && collision.gameObject.tag == "Chão"){
-            noChão=true;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChão && collision.gameObject.CompareTag("Chão"))
+        {
+            noChão = true;
         }
-     }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(message:"UPDATE");
-        float h = Input.GetAxis("Horizontal") ;
+        UnityEngine.Debug.Log("UPDATE");
+
+        float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
-        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
 
-if(Input.GetKeyDown(KeyCode.Space)&& noChão){
-    //pulo
-    source.Play();
-    rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
-    noChão = false;
-}
+        if (Input.GetKeyDown(KeyCode.Space) && noChão)
+        {
+            // Pulo
+            source.Play();
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChão = false;
+        }
 
-
-
-
-
-            if(transform.position.y < -10)
-        {SceneManager.LoadScene(SceneManager.GetActiveScene().name);}
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
